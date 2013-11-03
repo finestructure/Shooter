64aa61a8c896a58e790fc6b2e5f7b967cf529eed
Sven A. Schmidt <sas@feinstruktur.com>
2013-11-03 08:34:39 +0000

    Movable flame

--- Shooter/MyScene.m
+++ Shooter/MyScene.m
@@ -8,41 +8,45 @@
 
 #import "MyScene.h"
 
-@implementation MyScene
+#import "SKEmitterNode+Util.h"
 
--(id)initWithSize:(CGSize)size {    
+
+static const CGFloat FlameYOffset = 40;
+
+
+@implementation MyScene {
+    SKNode *_flame;
+}
+
+
+
+-(id)initWithSize:(CGSize)size
+{
     if (self = [super initWithSize:size]) {
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        NSString *path = [[NSBundle mainBundle] pathForResource:@"Flame" ofType:@"sks"];
-        SKEmitterNode *flame = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
-        flame.position = CGPointMake(CGRectGetMidX(self.frame), 40);
-        [self addChild:flame];
+        _flame = [SKEmitterNode emitterNodeWithParticleFileNamed:@"Flame"];
+        _flame.position = CGPointMake(CGRectGetMidX(self.frame), FlameYOffset);
+        [self addChild:_flame];
 
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+        CGPoint newLoc = CGPointMake(location.x, _flame.position.y);
+        _flame.position = newLoc;
     }
 }
+
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
 }
+
 
 @end
