1ef60c032b05b94808b40321ab9fd5fcfa6cfaaa
Sven A. Schmidt <sven@blinkbox.com>
2014-09-18 09:21:09 +0100

    Fix for Xcode-6.1b2

--- Shooter/ViewController.swift
+++ Shooter/ViewController.swift
@@ -39,9 +39,9 @@ class ViewController: UIViewController {
     
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
         } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
+            return Int(UIInterfaceOrientationMask.All.rawValue)
         }
     }
 
